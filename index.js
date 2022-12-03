6d86ceaaec361d97af2b2b5f4d3bc28f94301dc7
+ <+>
2022-12-03 12:01:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json));
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+    // h2.innerHTML = `<h2>${book.name}</h2>`;
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
@@ -15,3 +20,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
